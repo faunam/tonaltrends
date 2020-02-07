005d83981452e7920e941d18f6da54d49f07553a
Fauna Mahootian <flm45@cornell.edu>
2020-02-07 15:02:03 -0800

    fixed datetime and title bugs

--- process_gdelt.py
+++ process_gdelt.py
@@ -1,6 +1,7 @@
 from dateutil import parser
 from pyspark.sql.functions import col, udf, lit, explode
-from pyspark.sql.types import ArrayType, IntegerType, StringType
+from pyspark.sql.types import ArrayType, IntegerType, StringType, TimestampType
+import re
 
 # in docs, write what each of these are for the diff sources
 # should this be an enviro var? i need to reference it in mult files
@@ -10,10 +11,22 @@ entities = ["facebook", "amazon", "jeff bezos",
 
 
 def get_titl_and_auth(meta_xml, page_source):
-    # regex <PAGE_TITLE> __ </PAGE_TITLE>, <PAGE_AUTHORS> __ </PAGE_AUTHORS>,
+    # regex <PAGE_TITLE> hello </PAGE_TITLE>, <PAGE_AUTHORS> __ </PAGE_AUTHORS>,
     # returns a string
     # TODO: implement
-    return "title"
+    if meta_xml is None and page_source is None:
+        return "No title information"
+    if meta_xml is None:
+        return "Source: " + page_source
+    pattern = re.compile("<PAGE_TITLE>(.*)</PAGE_TITLE>")
+    search_result = re.search(pattern, meta_xml)
+    if search_result is None:
+        title = "No title information"
+    else:
+        title = search_result.group(1)
+    if page_source is None:
+        return title
+    return title + ", Source: " + page_source
 
 
 def convert_tone(tone_string):
@@ -24,7 +37,8 @@ def convert_tone(tone_string):
 
 def convert_datetime(news_date):
     # returns whatever format datetime is in
-    return parser.parse(str(news_date))  # TODO: fix
+    # TODO: fix
+    return parser.parse(str(news_date))
 
 
 def combine_mentions(people, orgs):
@@ -53,7 +67,7 @@ def process_gdelt(gdelt_df):
     gdelt_df = gdelt_df.withColumn("media", lit(
         "news"))  # if this doesnt work try udf
     # date -> utc
-    udf_datetime = udf(convert_datetime)
+    udf_datetime = udf(convert_datetime, TimestampType())  # StringType()
     gdelt_df = gdelt_df.withColumn("date", udf_datetime("date"))
     # tone -> just first num i think
     udf_tone = udf(convert_tone, IntegerType())
