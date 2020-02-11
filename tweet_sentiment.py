a6f59db81b3e4a2e16a2fc51ee1e05ca7ded85f1
Fauna Mahootian <flm45@cornell.edu>
2020-02-10 22:08:36 -0800

    making tweet sentiment score an integer, consistent with gdelt

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,9 +1,10 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from io import open
 
 
-def get_tweet_tone(tweet):
+def get_tweet_tone(text):
     # returns number between -100 and 100
     analyser = SentimentIntensityAnalyzer()
     # apparently vader doesn't need punctuation removal/stopword removal.
-    score = analyser.polarity_scores(tweet["text"])
-    return score["compound"]*100
+    score = analyser.polarity_scores(text)
+    return int(round(score["compound"]*100))
