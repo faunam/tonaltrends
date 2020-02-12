76b7bee081ddab01be9293f8b11c47c29e6b0e35
Fauna Mahootian <flm45@cornell.edu>
2020-02-11 19:44:47 -0800

    another iteration on processing twitter. VERY inefficient; modifies columnar data for all rows rather than the ones I care about (the ones mentioning my client entities)

--- process_twitter.py
+++ process_twitter.py
@@ -1,19 +1,23 @@
 from dateutil import parser
 import json
-from pyspark.sql.functions import col, udf, lit
-from pyspark.sql.types import StructType, StructField, ArrayType, IntegerType, StringType
+from pyspark.sql.functions import col, udf, lit, explode
+from pyspark.sql.types import StructType, StructField, ArrayType, IntegerType, StringType, TimestampType
+from pyspark.sql import DataFrame
 import re
 from unidecode import unidecode
+from functools import reduce
 
 import tweet_sentiment
 
 # in docs, write what each of these are for the diff sources
 # should this be an enviro var? i need to reference it in mult files
-columns = ["entity", "media", "date", "uniq_id", "tone", "mentions", "text"]
+columns = ["entity", "media", "date", "uniq_id", "tone", "text"]
 ent_twitter_handles = ["facebook", "amazon", "jeffbezos",
                        "realdonaldtrump", "sensanders", "berniesanders"]  # TODO: better soln than this
 twit_to_ent = {"facebook": "facebook", "amazon": "amazon", "jeffbezos": "jeff bezos",
                "realdonaldtrump": "donald trump", "sensanders": "bernie sanders", "berniesanders": "bernie sanders"}
+ent_to_twit = {"facebook": ["facebook"], "amazon": ["amazon"], "jeff bezos": ["jeffbezos"],
+               "donald trump": ["realdonaldtrump"], "bernie sanders": ["berniesanders", "sensanders"]}
 entities_extended = {"facebook": ["facebook"], "amazon": ["amazon"], "jeff bezos": ["jeff bezos", "bezos"],
                      "donald trump": ["donald trump", "trump"], "bernie sanders": ["bernie sanders", "bernie", "sanders"]}
 # TODO: automate this list creation ^
@@ -125,3 +129,106 @@ def format(spark, filepath):  # ent_twitter_handles was a param
                 continue
 
         return spark.createDataFrame(df_rows, columns)
+
+
+def get_hashtag_text(hashtags):
+    if hashtags is None:
+        return ""
+    return " ".join([hashtag["text"] for hashtag in hashtags])
+
+
+def get_mentions_text(mentions):
+    if mentions is None:
+        return ""
+    return " ".join([mention["screen_name"] for mention in mentions])
+
+
+def get_mentions_and_ent(df, spark):
+    # prepare relevant columns
+    # tags
+    udf_hashtag_text = udf(get_hashtag_text)
+    df = df.withColumn("hashtag_text", udf_hashtag_text(
+        "entities.hashtags"))
+    # @s
+    udf_mentions_text = udf(get_mentions_text)
+    df = df.withColumn("mention_text", udf_mentions_text(
+        "entities.user_mentions"))
+    df.createOrReplaceTempView("tweets_modded")
+
+    mentioned_dfs = []
+    for entity in entities:
+        possible_tag_refs = [ref.replace(" ", "")
+                             for ref in entities_extended[entity]]
+        possible_text_refs = entities_extended[entity]
+        possible_mention_refs = ent_to_twit[entity]
+        # select where any possible ref is in tags or mentions or text
+        # may need to create temporary view within this function ("all_tweets")
+        mentioned_df = spark.sql("SELECT * FROM tweets_modded WHERE hashtag_text RLIKE '"
+                                 + "|".join(possible_tag_refs) +
+                                 "' OR mention_text RLIKE '"
+                                 + "|".join(possible_mention_refs) +
+                                 "' OR text RLIKE '"
+                                 + "|".join(possible_text_refs) + "'")
+        mentioned_df = mentioned_df.withColumn("entity", lit(entity))
+        mentioned_dfs.append(mentioned_df)
+
+    return reduce(DataFrame.unionAll, mentioned_dfs)
+
+
+def get_tone(text):
+    return tweet_sentiment.get_tweet_tone(text)
+
+
+def get_text(text, retweeted):
+    try:
+        return unidecode(retweeted["extended_tweet"]["full_text"])
+    except:
+        return unidecode(text)
+    # if fulltext is None:
+    #     return text
+    # return fulltext["full_text"]
+
+
+def convert_datetime(date):
+    # returns whatever format datetime is in
+    return parser.parse(str(date))
+
+
+def process_twitter(twitter_df, spark):
+    # could combine these udfs with above functions to make code cleaner
+    # uniq_id
+    twitter_df = twitter_df.withColumnRenamed("id", "uniq_id")
+    # source (news)
+    twitter_df = twitter_df.withColumn("media", lit(
+        "twitter"))  # if this doesnt work try udf
+    # date -> utc
+    udf_datetime = udf(convert_datetime, TimestampType())  # StringType()
+    twitter_df = twitter_df.withColumn("date", udf_datetime("created_at"))
+    # text
+    udf_text = udf(get_text)
+    twitter_df = twitter_df.withColumn("text", udf_text(
+        "text", "retweeted_status"))
+    # tone
+    udf_tone = udf(get_tone, IntegerType())
+    twitter_df = twitter_df.withColumn("tone", udf_tone("text"))
+    twitter_df.select("tone").show(2)
+    #mentions, entity
+    twitter_df = get_mentions_and_ent(twitter_df, spark)
+
+# TODO: filter by entity before doing other transformatinos so it's less computationally intense
+    return twitter_df.select(*columns)
+
+
+def format2(spark, s3_filepath):  # entities used to be a param
+    twitter_df = spark.read.json(
+        s3_filepath)  # might want to think about partitioning
+    # https://stackoverflow.com/questions/37445054/spark-difference-when-read-in-gz-and-bz2
+
+    # could filter for mentions here maybe? not sure how to handle extended tweets conditional
+    twitter_df.createOrReplaceTempView("all_tweets")
+
+    twitter_df = spark.sql("SELECT * FROM all_tweets WHERE lang = 'en'")
+
+    twitter_df = process_twitter(twitter_df, spark)
+
+    return twitter_df
